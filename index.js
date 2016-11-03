65f6ca40811e34e02d06a12895504c8b1e1b9b52
Haoyang Xu <Haoyang Xu>
2016-11-03 12:24:41 +0800

    use collections and permalinks

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Dependencies
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown-it'),
-    layouts = require('metalsmith-layouts')
+    layouts = require('metalsmith-layouts'),
+    collection = require('metalsmith-collections'),
+    permalink = require('metalsmith-permalinks')
 
 // default callback on errors
 var err = function (err) { if(err) console.log(err) }
@@ -14,8 +16,37 @@ var layoutOptions = {
     partials:"templates/partials"
 }
 
-Metalsmith(__dirname)
+var metalsmith = new Metalsmith(__dirname)
+    .use(collection({
+	pages: {
+	    pattern: "content/pages/*.md"
+	},
+	blog: {
+	    pattern: "content/blog/*.md",
+	    sortBy: "date",
+	    reverse: true
+	},
+	reviews: {
+	    pattern: "content/reviews/*.md"
+	},
+	links: {
+	    pattern: "content/links/*.md"
+	}}))
     .use(markdown())
     .use(layouts(layoutOptions))
+    .use(permalink({
+	pattern: ':title',
+	linksets: [{
+	    match: { collection: 'blog' },
+	    pattern: 'blog/:date/:title'
+	},{
+	    match: { collection: 'reviews' },
+	    pattern: 'reviews/:title'
+	},{
+	    match: { collection: 'links' },
+	    pattern: 'links/:title'
+	},{
+	    match: { collection: 'pages' },
+	    pattern: ':title'}]}))
     .destination('./build')
     .build(err)
