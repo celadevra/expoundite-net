255a477618d51b75a69787a93a3323d8f69eea2c
Haoyang Xu <Haoyang Xu>
2016-11-03 11:28:15 +0800

    layout options

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+// Dependencies
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown-it'),
-    templates = require('metalsmith-templates')
+    layouts = require('metalsmith-layouts')
 
+// default callback on errors
 var err = function (err) { if(err) console.log(err) }
+
+// metalsmith-layouts options
+var layoutOptions = {
+    engine: "mustache",
+    default:"default.mustache",
+    directory:"templates",
+    partials:"templates/partials"
+}
 
 Metalsmith(__dirname)
     .use(markdown())
+    .use(layouts(layoutOptions))
     .destination('./build')
     .build(err)
