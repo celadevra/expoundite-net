6a9ec393ac49a223c619cb8d07eca87dff2b063f
Haoyang Xu <Haoyang Xu>
2016-11-03 10:43:10 +0800

    add dep

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var Metalsmith = require('metalsmith')
+var Metalsmith = require('metalsmith'),
+    markdown = require('metalsmith-markdown-it'),
+    templates = require('metalsmith-templates')
+
+var err = function (err) { if(err) console.log(err) }
 
 Metalsmith(__dirname)
+    .use(markdown())
     .destination('./build')
-    .build()
+    .build(err)
